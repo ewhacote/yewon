ff91a33107d705511aa75376eb745f2f19aaebab
eyewon <111197714+eyewon@users.noreply.github.com>
2023-07-24 23:18:44 +0900

    [0724] 유사칸토어비트열

---
+++ 유사 칸토어 비트열/solution.py
@@ -0,0 +1,28 @@
+#문제 https://school.programmers.co.kr/learn/courses/30/lessons/148652
+
+def solution(n, l, r):
+    return count_1(r) - count_1(l-1)
+
+
+def count_1(idx):
+    if idx <= 5:
+        return '11011'[:idx].count('1')
+
+    power = 1
+    while 5 ** (power + 1) <= idx:
+        power += 1
+
+    base = 5 ** power
+    section, remainder = divmod(idx, base)
+    if section == 0:
+        return count_1(remainder)
+    elif section == 1:
+        return (4 ** power) + count_1(remainder)
+    elif section == 2:
+        return 2 * (4 ** power)
+    elif section == 3:
+        return 2 * (4 ** power) + count_1(remainder)
+    elif section == 4:
+        return 3 * (4 ** power) + count_1(remainder)
+    else: # section == 5
+        return 4 * (4 ** power)
