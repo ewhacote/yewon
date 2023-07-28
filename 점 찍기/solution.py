7450fc79a52567463d2dbb3ad1663ea35d1eba5c
eyewon <111197714+eyewon@users.noreply.github.com>
2023-07-28 23:25:47 +0900

    [0728] 점찍기

---
+++ 점 찍기/solution.py
@@ -0,0 +1,10 @@
+# 문제 https://school.programmers.co.kr/learn/courses/30/lessons/140107
+
+import math
+def solution(k, d):
+    answer = 0
+    # x 기준으로 세기
+    for x in range(0, d + 1, k):
+        res_d = math.floor(math.sqrt(d*d - x*x))
+        answer += (res_d // k) + 1
+    return answer 
