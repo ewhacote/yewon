1b280c0440479b170b3bb385dc1c0ed86835b0ca
eyewon <111197714+eyewon@users.noreply.github.com>
2023-10-18 23:49:14 +0900

    Create 큰수만들기.py

---
+++ 17주차/큰수만들기.py
@@ -0,0 +1,18 @@
+# 문제 https://school.programmers.co.kr/learn/courses/30/lessons/42883
+
+def solution(number, k):
+    answer = []
+
+    for i in number:
+        if not answer:
+            answer.append(i)
+            continue
+        while answer[-1] < i and k > 0:
+            answer.pop()
+            k -= 1
+            if not answer or k <= 0:
+                break
+        answer.append(i)
+        if len(answer) == len(number) - k:
+            break
+    return ''.join(answer)
