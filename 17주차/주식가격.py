a13e713d2d6fcf3359c684166bf3776f5df19c22
eyewon <111197714+eyewon@users.noreply.github.com>
2023-10-31 21:41:05 +0900

    Create 주식가격.py

---
+++ 17주차/주식가격.py
@@ -0,0 +1,14 @@
+# 문제 https://school.programmers.co.kr/learn/courses/30/lessons/42584
+
+def solution(prices):
+
+    times = [0] * len(prices)
+    stack = []
+    for i in range(len(prices)):
+        for s,idx in stack:
+            times[idx] += 1
+        while stack and stack[-1][0] > prices[i]:
+            stack.pop()
+        stack.append((prices[i], i))
+
+    return times
