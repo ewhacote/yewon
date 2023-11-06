ec91589d5e2a839190fa42bbe195de8abb26cd0a
eyewon <111197714+eyewon@users.noreply.github.com>
2023-11-06 23:15:43 +0900

    Create n진수게임.py

---
+++ 17주차/n진수게임.py
@@ -0,0 +1,16 @@
+# 문제 https://school.programmers.co.kr/learn/courses/30/lessons/17687
+
+def solution(n, t, m, p):
+    answer = "0"
+    notation = "0123456789ABCDEF"
+
+    for num in range(1, m * t):
+        result = ""
+        while num > 0:
+            num, remainder = divmod(num, n)
+            result += notation[remainder]
+
+        answer += result[::-1]
+
+
+    return answer[p-1::m][:t]
